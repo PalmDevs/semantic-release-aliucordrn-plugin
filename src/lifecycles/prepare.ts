2d3217550329759f0027147a4305986d40c93075
Palm <palmpasuthorn@gmail.com>
2023-04-07 17:44:30 +0700

    fix: issues when resolving paths, add more logging

--- src/lifecycles/prepare.ts
+++ src/lifecycles/prepare.ts
@@ -2,7 +2,6 @@ import fs = require('fs');
 import type { Context } from 'semantic-release';
 import type PluginConfig from '../types/PluginConfig.js';
 
-const { readFileSync, writeFileSync } = fs;
 const prepare = function prepare(
     { manifestFile }: Required<PluginConfig>,
     context: Context
@@ -10,10 +9,15 @@ const prepare = function prepare(
     const { logger } = context;
     logger.log('Loading manifest file: %s', manifestFile);
 
-    const manifest = readFileSync(manifestFile, 'utf8');
+    const manifest = fs.readFileSync(manifestFile, 'utf8');
+
+    logger.debug('File contents:\n%s', manifest);
+
     const versionField = manifest.match(prepare.VersionFieldRegExp);
     if (!versionField)
-        throw new Error(`Cannot find version field in manifest file`);
+        throw new AggregateError([
+            `Cannot find version field in manifest file`,
+        ]);
 
     const [wholeString, version] = versionField;
     const newVersionFieldString = wholeString.replace(
@@ -22,7 +26,11 @@ const prepare = function prepare(
     );
 
     const newManifest = manifest.replace(wholeString, newVersionFieldString);
-    writeFileSync(manifestFile, newManifest, 'utf8');
+
+    logger.debug('New file contents:\n%s', newManifest);
+    logger.debug('Writing to file %s', manifestFile);
+
+    fs.writeFileSync(manifestFile, newManifest, 'utf8');
 };
 
 prepare.VersionFieldRegExp = /"version"\s*:\s*"(.+)"/;
--- src/lifecycles/verifyConditions.ts
+++ src/lifecycles/verifyConditions.ts
@@ -1,21 +1,27 @@
 import fs = require('fs');
 import path = require('path');
+import type { Config, Context } from 'semantic-release';
 import type PluginConfig from '../types/PluginConfig.js';
 
-const { existsSync } = fs;
-const { resolve: resolvePath } = path;
-
 export = function verifyConditions(
-    pluginConfig: PluginConfig | null | undefined
+    pluginConfig: PluginConfig | null | undefined,
+    context: Config & Context
 ) {
+    const { logger } = context;
+
     if (!pluginConfig) pluginConfig = {};
-    pluginConfig.manifestFile = resolvePath(
-        process.cwd(),
+    pluginConfig.manifestFile = path.join(
+        context.cwd ?? process.cwd(),
         pluginConfig.manifestFile ?? 'manifest.json'
     );
 
-    if (!existsSync(pluginConfig.manifestFile))
-        throw new Error(
-            `Cannot find manifest file: ${pluginConfig.manifestFile}`
-        );
+    logger.log(
+        'Checking if manifest file exists: %s',
+        pluginConfig.manifestFile
+    );
+
+    if (!fs.existsSync(pluginConfig.manifestFile))
+        throw new AggregateError([
+            `Cannot find manifest file: ${pluginConfig.manifestFile}`,
+        ]);
 };
